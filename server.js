ae14ffcd01372a10621ed05eb55d2ddc07e46eed
UROALSAADI <s223646475@deakin.edu.au>
2024-09-02 12:50:17 +0500

    Updated index.html and server.js for Socket.io functionality

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const http = require('http');
 const express = require('express');
+const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+app.use(express.static(__dirname));
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-
-  setInterval(() => {
-    socket.emit('number', parseInt(Math.random() * 10));
-  }, 1000);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(8080, () => {
-  console.log('Server is running on port 8080');
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10));
+    }, 1000);
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
